5c99af2d8e3c8c12735d5b7f72e7ef4265c85700
Aaron Deibele <aaron.deibele@capco.com>
2022-04-28 12:14:20 -0400

    Used overflow handler to generate prettier output

--- run.rb
+++ run.rb
@@ -1,26 +1,27 @@
 require './supervisor'
 $demand = 5
 $delay = 1
-$barbers = 5
+$barbers = 3
 $total_customers = 50
-$chairs = 10
+$chairs = 5
 
 def add_call(identifier)
   @supervisor.queue(Proc.new do
-    puts "#{identifier} started"
-    sleep(rand(1..3) * $delay)
-    puts "  #{identifier} finished"
+    puts "  #{identifier} started"
+    sleep(rand(0.5..2.0) * $delay)
+    puts "    #{identifier} finished"
   end)
 end
 
-@supervisor = Supervisor.new($barbers, max_queue_length: $chairs)
+@supervisor = Supervisor.new($barbers, max_queue_length: $chairs, overflow_handler: lambda { |_job| print ": no seat available" })
 @num = Semaphore.new
 while @num.peek + 1 < $total_customers do
   sleep(rand(0.0..3.0) / $demand)
   Thread.new do
     num = @num.signal
+    print("#{num} arrived")
     add_call(num)
-    puts("#{num} arrived")
+    print("\n")
   end
 end
 @supervisor.wait
